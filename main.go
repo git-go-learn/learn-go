477789e81161707f84799b660f9cd090405e5f85
git-go-learn <sauravubuntu@gmail.com>
2025-02-22 02:59:50 +0530

    git command

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+)
 
-func main () {
-	fmt.Println("Hello to go")
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Enter commit message: ")
+	scanner.Scan()
+	commitMessage := scanner.Text()
+
+	fmt.Print("Enter branch name to push (e.g., main): ")
+	scanner.Scan()
+	branchName := scanner.Text()
+
+	// Run git add .
+	exec.Command("git", "add", ".").CombinedOutput()
+
+	// Run git commit -m "commitMessage"
+	exec.Command("git", "commit", "-m", commitMessage).CombinedOutput()
+
+	// Run git push origin branchName
+	exec.Command("git", "push", "origin", branchName).CombinedOutput()
+	
+	fmt.Println("Commands executed.")
 }
